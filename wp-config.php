75895fb5eea84b04fea2c286f60d3ec756426a0b
Lucas Hopman <lucas.hopman@gmail.com>
2018-12-20 13:29:48 +0100

    wp-config.php bewerkt

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'Drone world');
+define('DB_NAME', 'drone_world');
 
 /** MySQL database username */
 define('DB_USER', 'root');
